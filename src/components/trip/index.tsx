26b4dca818ada8669204db6d8124251614ea5b55
R0ixy <nikita6kom@gmail.com>
2022-07-17 13:16:04 +0300

    added redirect to main page if wrong trip id in url params

--- src/components/trip/index.tsx
+++ src/components/trip/index.tsx
@@ -1,38 +1,46 @@
-import {useParams} from 'react-router-dom';
 import {useState} from "react";
+import {useParams, Navigate} from 'react-router-dom';
+import {Modal} from "../modal";
 import trips from '../../data/trips.json';
 import {iTrip} from "../mainPage/interfaces/iTrip";
-import {Modal} from "../modal";
 
 export const Trip = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const {id} = useParams();
-    const trip = trips.find((trip) => trip.id === id) as iTrip;
+    const trip: iTrip | undefined = trips.find((trip) => trip.id === id);
 
-    return (
-        <main className="trip-page">
-            <h1 className="visually-hidden">Travel App</h1>
-            <div className="trip">
-                <img src={trip.image} className="trip__img" alt="trip"/>
-                <div className="trip__content">
-                    <div className="trip-info">
-                        <h3 className="trip-info__title">{trip.title}</h3>
-                        <div className="trip-info__content">
-                            <span className="trip-info__duration"><strong>{trip.duration}</strong> days</span>
-                            <span className="trip-info__level">{trip.level}</span>
+
+    if (!trip) {
+        return (<Navigate to="/" replace={true}/>);
+
+    } else {
+
+        return (
+            <main className="trip-page">
+                <h1 className="visually-hidden">Travel App</h1>
+                <div className="trip">
+                    <img src={trip.image} className="trip__img" alt="trip"/>
+                    <div className="trip__content">
+                        <div className="trip-info">
+                            <h3 className="trip-info__title">{trip.title}</h3>
+                            <div className="trip-info__content">
+                                <span className="trip-info__duration"><strong>{trip.duration}</strong> days</span>
+                                <span className="trip-info__level">{trip.level}</span>
+                            </div>
                         </div>
+                        <div className="trip__description">
+                            {trip.description}
+                        </div>
+                        <div className="trip-price">
+                            <span>Price</span>
+                            <strong className="trip-price__value">{trip.price} $</strong>
+                        </div>
+                        <button className="trip__button button" onClick={() => setIsModalOpen(true)}>Book a trip
+                        </button>
+                        {isModalOpen && <Modal trip={trip} setIsModalOpen={setIsModalOpen}/>}
                     </div>
-                    <div className="trip__description">
-                        {trip.description}
-                    </div>
-                    <div className="trip-price">
-                        <span>Price</span>
-                        <strong className="trip-price__value">{trip.price} $</strong>
-                    </div>
-                    <button className="trip__button button" onClick={() => setIsModalOpen(true)}>Book a trip</button>
-                    {isModalOpen && <Modal trip={trip} setIsModalOpen={setIsModalOpen}/>}
                 </div>
-            </div>
-        </main>
-    );
+            </main>
+        );
+    }
 }
